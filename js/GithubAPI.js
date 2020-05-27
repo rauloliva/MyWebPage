44fb3338fc190b7df08b9df7a78abb096f3db5c2
Raul Oliva <oliva.raul12@gmail.com>
2020-05-26 21:05:41 -0500

    Merging code from Development branch

--- js/GithubAPI.js
+++ js/GithubAPI.js
@@ -11,6 +11,8 @@ async function getRepos(cb){
 }
 
 /**
+ * Displays the containers with the repos's info
+ * in the content element
  * 
  * @param {Array} repos 
  */
@@ -22,17 +24,28 @@ function showRespos(repos){
 }
 
 /**
+ * Build the container with some info about
+ * the retrieved repos
  * 
  * @param {Object} repo 
  */
 function buildRepoContainer(repo){
+    var dateObj = new Date(repo.created_at)
+    var date = (dateObj.getDate() + 1 ) + "/" + (dateObj.getMonth() + 1) + "/" + dateObj.getFullYear()
     var container = `
         <div class='repo__container'>
-            <h4>Respository Name: ${repo.name}</h4>
-            <h4>
-                <a href='${repo.html_url}'>Go to Github Repository</a>
+            <h4 class='repo__container-header'>
+                Respository Name: 
+                <span class='projectName'>${repo.name}</span>
             </h4>
-            <h4>Respository Description: ${repo.description}</h4>
+            <h4 class='repo__container-header'>
+                <a class='repo__container-link' href='${repo.html_url}'>Go to Github Repository</a>
+            </h4>
+            <h4 class='repo__container-header'>Respository Description:</h4>
+            <p class='content__paragraph'>
+                ${repo.description}.
+            </p>
+            <p class='content__paragraph'>This project was created in ${date}</p>
         </div>
         <hr>
     `
